c6834485f2200eab76af56a81825d909a322a99f
Maxim Yurevich <maxim.yurewitch@gmail.com>
2026-03-27 16:17:17 +0300

    feat: add WinGet CommandNotFound module and Zellij tabula plugin to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ Set-PSReadLineOption -EditMode Vi
 Set-Alias vi nvim
 Set-Alias vim nvim
 
+
 function Invoke-Starship-TransientFunction
 {
   &starship module character
@@ -17,3 +18,11 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 mise activate pwsh | Out-String | Invoke-Expression
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+
+. "$env:APPDATA\Zellij\zellij-tabula.plugin.ps1"
